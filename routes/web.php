1a6da19c5a3c6068a23a6a55ac5ff9e58bab4057
Eric DM <eric@digitalmanagement.fr>
2022-10-03 08:28:35 +0000

    mise en place de l'interface utilisateur

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Admin\PostController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\FrontController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [FrontController::class, 'index']);
+Route::resource('posts', PostController::class);
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
